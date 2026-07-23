f7c1a065b88bbc5ff76deffcbf0356d2c59d3d3f
Ehtasham Daud Satti <ehtashamdaudsatti@gmail.com>
2026-07-22 23:17:07 -0700

    Fix Vercel routes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-// app.use("/api/peterson", petersonRoutes);
+// Home Route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Peterson Backend Running Successfully 🚀"
+  });
+});
+
+// Test API
+app.get("/api/peterson", (req, res) => {
+  res.json({
+    success: true,
+    title: "Peterson Solution",
+    message: "Backend is working on Vercel"
+  });
+});
 
 module.exports = app;
